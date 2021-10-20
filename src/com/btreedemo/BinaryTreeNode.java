12b9da7c17f3522f154d8e31e3d021082ec56c9e
praveen-java <javaenthusiastic.praveen@gmail.com>
2021-10-20 12:19:36 +0530

    Create BinaryTreeNode.java

---
+++ src/com/btreedemo/BinaryTreeNode.java
@@ -0,0 +1,30 @@
+package com.btreedemo;
+
+public class BinaryTreeNode {
+	public int data;
+	public BinaryTreeNode left;
+	public BinaryTreeNode right;
+	public BinaryTreeNode(int data) {
+		this.data=data;
+		left = right = null;
+	}
+	public int getData() {
+		return data;
+	}
+	public void setData(int data) {
+		this.data = data;
+	}
+	public BinaryTreeNode getLeft() {
+		return left;
+	}
+	public void setLeft(BinaryTreeNode left) {
+		this.left = left;
+	}
+	public BinaryTreeNode getRight() {
+		return right;
+	}
+	public void setRight(BinaryTreeNode right) {
+		this.right = right;
+	}
+	
+}
